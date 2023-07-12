d9cb2c5626b8239c8f33bd0cae894095deabc283
Mishator <voyteh89@mail.ru>
2023-07-12 21:53:04 +0300

    Создали метод withMaxSalary.

--- src/main/java/com/example/homeworknewcollections25/controller/DepartmentController.java
+++ src/main/java/com/example/homeworknewcollections25/controller/DepartmentController.java
@@ -1,10 +1,23 @@
 package com.example.homeworknewcollections25.controller;
 
 
+import com.example.homeworknewcollections25.entity.Employee;
+import com.example.homeworknewcollections25.service.DepartmentService;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/departments")
 public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    public Employee withMaxSalary(@RequestParam Integer departmentId) {
+        return departmentService.withMaxSalary(departmentId);
+    }
 }
--- src/main/java/com/example/homeworknewcollections25/service/DepartmentService.java
+++ src/main/java/com/example/homeworknewcollections25/service/DepartmentService.java
@@ -1,7 +1,25 @@
 package com.example.homeworknewcollections25.service;
 
+import com.example.homeworknewcollections25.entity.Employee;
+import com.example.homeworknewcollections25.exception.EmployeeNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Service
 public class DepartmentService {
+
+    private final EmployeeService employeeService;
+
+    public DepartmentService(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    public Employee withMaxSalary(Integer departmentId) {
+        List<Employee> employees = employeeService.getAll();
+        return employees.stream()
+                .max(Comparator.comparing(Employee::getSalary))
+                .orElseThrow(() -> new EmployeeNotFoundException("Сотрудник не найден"));
+    }
 }
